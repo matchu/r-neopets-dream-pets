e057c6fe09d3c34bed0d3fe35773bd5218f0359e
Matchu <matchusian@gmail.com>
2015-06-12 14:13:55 -0400

    more nav polish

--- app/helpers/pet_requests_helper.rb
+++ app/helpers/pet_requests_helper.rb
@@ -1,8 +1,4 @@
 module PetRequestsHelper
-  def can_create_pet_requests?
-    user_signed_in?
-  end
-
   def can_edit_pet_request?(pet_request)
     user_signed_in? && current_user.can_edit_pet_request?(pet_request)
   end
