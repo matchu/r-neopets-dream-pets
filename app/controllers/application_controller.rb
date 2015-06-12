45b48665fbc21be1be2233a8122ac1c3b594a400
Matchu <matchusian@gmail.com>
2015-06-12 00:10:49 -0400

    omniauth via developer; store auth hash in session

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  helper_method :auth?, :current_auth_hash
+
+  def current_auth_hash
+    session[:auth_hash]
+  end
+
+  def auth?
+    session.has_key? :auth_hash
+  end
 end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,26 @@
+class SessionsController < ApplicationController
+  # External services redirect to the callback, sometimes with a POST,
+  # but they don't know the authenticity token. So, don't check.
+  protect_from_forgery except: :create
+
+  def create
+    session[:auth_hash] = auth_hash
+    redirect_to root_path
+  end
+
+  def destroy
+    # TODO: protect from forgery?
+    session.delete :auth_hash
+    redirect_to root_path
+  end
+
+  def index
+    # Auth debug method for development
+  end
+
+  protected
+
+  def auth_hash
+    request.env['omniauth.auth']
+  end
+end
