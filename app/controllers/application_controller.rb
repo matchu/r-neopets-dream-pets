a5f3e69e4b0f7d7bb571af74b576cbf3629373d0
Matchu <matchusian@gmail.com>
2015-06-12 00:35:52 -0400

    cute profile link if current provider is Reddit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,10 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  helper_method :auth?, :auth_providers, :current_auth_name, :auth_providers
+  helper_method :auth?, :current_auth_provider, :current_auth_name
 
   def auth?
     session.has_key? :auth_hash
+  end
+
+  def current_auth_provider
+    ActiveSupport::StringInquirer.new current_auth_hash['provider']
   end
 
   def current_auth_name
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def reddit_user_path(name)
+    "https://www.reddit.com/user/#{name}"
+  end
 end
