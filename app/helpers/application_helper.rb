efff8bdd36b94d15b42355bd9ebaeedbe7e117b8
Matchu <matchusian@gmail.com>
2015-06-12 00:38:34 -0400

    minor refactor to user link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 module ApplicationHelper
+  def link_to_user(provider, name)
+    if provider.reddit?
+      link_to name, reddit_user_path(name)
+    else
+      name
+    end
+  end
+
   def reddit_user_path(name)
     "https://www.reddit.com/user/#{name}"
   end
