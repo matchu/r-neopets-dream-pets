9d8d2d37766556accdc6e502100f208522bf68f7
Matt Dunn-Rankin <mattdunnrankin@khanacademy.org>
2018-01-30 15:24:51 -0800

    Revert "whoops, discord post "author" name is markdown!"
    This reverts commit 4d0c4618786698092b26933b27d5d93ba74c712b.
    
    Turns out we were wrong! :P

--- app/jobs/post_in_discord_job.rb
+++ app/jobs/post_in_discord_job.rb
@@ -39,7 +39,8 @@ class PostInDiscordJob < ApplicationJob
         color: 0xFF00FF,
         timestamp: pet_request.created_at.utc.iso8601,
         author: {
-          name: escape_markdown("Dream Pet added by /u/#{pet_request.creator.name}"),
+          # This attribute won't be presented as Markdown, so isn't escaped.
+          name: "Dream Pet added by /u/#{pet_request.creator.name}",
           url: "https://www.reddit.com/user/#{pet_request.creator.name}",
         },
         footer: {
